715172164fd45fb1af997b463bd92506a956a0de
Md Faisal Hossain <faisalh.iu@gmail.com>
2021-09-30 23:37:58 +0200

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import react, { useState } from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
@@ -35,15 +37,27 @@ const options = [
 ];
 
 const App = () => {
-  // const [selected, setSelected] = useState(options[1]);
+  const [selected, setSelected] = useState(options[1]);
   return (
     <div>
-      <Translate />
-      {/* <Dropdown
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      /> */}
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
